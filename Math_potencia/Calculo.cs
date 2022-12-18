659047feec86b336c6ef960cae5051d3d6dd4d2a
Bruno Tassi <brunoxtassi923@gmail.com>
2022-12-18 18:22:36 -0300

    commit classe math

---
+++ Math_potencia/Calculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_potencia
+{
+    class Calculo
+    {
+        public void Potencia(int x , int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+
+        public void seno (double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+
+            Console.WriteLine($"Seno de {angulo} é igual {Math.Round(seno,4)}");
+        }
+        public void cosseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+
+            Console.WriteLine($"cosseno de {angulo} é igual {Math.Round(coseno,4)}");
+        }
+        public void tangente (double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+
+            Console.WriteLine($"tangente de {angulo} é igual {Math.Round(tangente,4)}");
+        }
+    }
+}
---
+++ Math_potencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_potencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calc = new Calculo();
+            //calc.Potencia(3, 3);
+
+            calc.seno(30);
+            calc.cosseno(30);
+            calc.tangente(30);
+
+            Console.ReadKey();
+        }
+    }
+}
